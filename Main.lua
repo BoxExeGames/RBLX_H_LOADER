732349c3bdb1896c294a0ff24b8b32bfb8241718
LightningWare_YT <146706767+BoxExeGames@users.noreply.github.com>
2025-10-02 20:43:01 +1000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -13,3 +13,50 @@ local function RequireModuleFromID(ID)
 	return module
 end
 
+--Replaces textures with H
+local texture = "http://www.roblox.com/asset/?id=1460469885"
+
+local d = game.Workspace:GetDescendants()
+
+for i = 1, #d do 
+	if d[i].ClassName == "Decal" or d[i].ClassName == "Texture" and d[i].Texture ~= texture then
+		d[i]:Destroy()
+	end
+	
+	if (d[i].className == "Part") or (d[i].className == "MeshPart") or (d[i].className == "UnionOperation") or (d[i].className == "WedgePart") or (d[i].className == "CornerWedgePart")or (d[i].className == "TrussPart") or (d[i].ClassName == "SpawnLocation") and (d[i].Name ~= "PrimaryPart") then
+		local sus = Instance.new("Decal", d[i])
+		
+		sus.Texture = texture
+		sus.Face = "Front"
+		
+		local sus = Instance.new("Decal", d[i])
+		
+		sus.Texture = texture
+		sus.Face = "Back"
+		
+		local sus = Instance.new("Decal", d[i])
+		
+		sus.Texture = texture
+		sus.Face = "Left"
+		
+		local sus = Instance.new("Decal", d[i])
+		
+		sus.Texture = texture
+		sus.Face = "Right"
+		
+		local sus = Instance.new("Decal", d[i])
+		
+		sus.Texture = texture
+		sus.Face = "Top"
+		
+		local sus = Instance.new("Decal", d[i])
+		
+		sus.Texture = texture
+		sus.Face = "Bottom"
+		
+		d[i]:BreakJoints()
+		d[i].BrickColor = BrickColor.new("Institutional white")
+		wait(0.1)
+	end 
+end
+--end here
