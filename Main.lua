195565c8813d2e86c42c972e77c4765061e5a59f
LightningWare_YT <146706767+BoxExeGames@users.noreply.github.com>
2025-10-02 20:00:30 +1000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,15 @@
 warn("Initializing H Loader")
 print("h")
+
+local function GetModuleID(module)
+	local module = require(module)
+	
+	return print(module)
+end
+
+local function RequireModuleFromID(ID)
+	local module = require(ID)
+	
+	return module
+end
+
